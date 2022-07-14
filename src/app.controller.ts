30a47e97ba53cdd7ecf1a0f48d6b8ccaa26a24ca
rmagur1203 <rmagur1203@naver.com>
2022-07-14 16:03:08 +0900

    feat: implement hello service for example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { Hello } from './interfaces/hello';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @GrpcMethod('AppService', 'getHello')
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): Hello {
+    return { message: this.appService.getHello() };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.GRPC,
     options: {
+      url: 'localhost:50051',
       package: 'app',
-      protoPath: 'src/app.proto',
+      protoPath: join(__dirname, 'app.proto'),
     },
   });
   await app.listen();
